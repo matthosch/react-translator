c25d780582da9fdad6512dd167c15eba6cad63d5
Matt Hosch <matt_hosch@hotmail.com>
2020-11-09 17:22:28 -0600

    Added translation components, styling

---
+++ src/components/translate.js
@@ -0,0 +1,46 @@
+import React, { useEffect, useState } from "react";
+import axios from "axios";
+
+const doTranslation = async (input, languageCode, cancelToken) => {
+  try {
+    const { data } = await axios.post(
+      "https://translation.googleapis.com/language/translate/v2?key=" + process.env.REACT_APP_TRANSLATE_API_KEY,
+      {
+        q: input,
+        target: languageCode
+      },
+      { cancelToken: cancelToken.token }
+    );
+
+    return data.data.translations[0].translatedText;
+  } catch (err) {
+    return "";
+  }
+};
+
+export default function Translate({ language, text }) {
+  const [translated, setTranslated] = useState("");
+
+  useEffect(() => {
+    if (!text) {
+      return;
+    }
+
+    const cancelToken = axios.CancelToken.source();
+
+    doTranslation(text, language, cancelToken).then(setTranslated);
+
+    return () => {
+      try {
+        cancelToken.cancel();
+      } catch (err) {}
+    };
+  }, [text, language]);
+
+  return (
+    <div>
+      <label className="label">Output</label>
+      <h1 className="title">{translated}</h1>
+    </div>
+  );
+};
